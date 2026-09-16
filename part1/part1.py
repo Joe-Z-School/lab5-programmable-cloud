7bffe5b459dfbc10134a824d5e7ca3c828ce149e
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-09-16 18:25:58 +0000

    Adding starter code and startup script

--- part1/part1.py
+++ part1/part1.py
@@ -7,10 +7,23 @@ from pprint import pprint
 
 import googleapiclient.discovery
 import google.auth
+import sys
+sys.path.append('../')
+#from cloud.parts import *
 
 credentials, project = google.auth.default()
 service = googleapiclient.discovery.build('compute', 'v1', credentials=credentials)
+zone = "us-west1-b"
+fwPort = 5000
+fwName = 'allow-{}'.format(fwPort)
+instanceName = 'blog'
+nodeType = 'f1-micro'
 
+
+if not project or project == '':
+    print("No project found, or need to run 'gcloud config set project <project-name>'")
+
+#
 #
 # Stub code - just lists all instances
 #
@@ -21,3 +34,25 @@ def list_instances(compute, project, zone):
 print("Your running instances are:")
 for instance in list_instances(service, project, 'us-west1-b'):
     print(instance['name'])
+
+create_firewall_rule(service, project, fwPort, fwName)
+
+try:
+    operation = create_instance(service, project, zone, instanceName,
+        nodeType = nodeType,
+        scriptFile = 'startup-script.sh',
+        fwName = fwName)
+    wait_for_operation(service, project, zone, operation['name'])
+except googleapiclient.errors.HttpError as exp:
+    print("An instance may already be running")
+    print(exp)
+
+try:
+    instanceResponse = service.instances().get(project=project, zone=zone, instance=instanceName).execute()
+    if instanceResponse:
+        instanceIP = instanceResponse['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+        print(f"The blog is runnin at http://{instanceIP}:{fwPort}")
+except googleapiclient.errors.HttpError as exp:
+    print(f"Unable to locate instance {instanceName} to get the IP address")
+    print(exp)
+
