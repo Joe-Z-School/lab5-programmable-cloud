0a4b55317f181df99c1f6060d276a1851279b740
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-09-18 18:58:03 +0000

    Completed initial start up of blog instance

--- part1/part1.py
+++ part1/part1.py
@@ -8,51 +8,104 @@ from pprint import pprint
 import googleapiclient.discovery
 import google.auth
 import sys
-sys.path.append('../')
-#from cloud.parts import *
-
-credentials, project = google.auth.default()
-service = googleapiclient.discovery.build('compute', 'v1', credentials=credentials)
-zone = "us-west1-b"
-fwPort = 5000
-fwName = 'allow-{}'.format(fwPort)
-instanceName = 'blog'
-nodeType = 'f1-micro'
 
 
-if not project or project == '':
-    print("No project found, or need to run 'gcloud config set project <project-name>'")
-
-#
-#
-# Stub code - just lists all instances
-#
 def list_instances(compute, project, zone):
     result = compute.instances().list(project=project, zone=zone).execute()
     return result['items'] if 'items' in result else None
 
-print("Your running instances are:")
-for instance in list_instances(service, project, 'us-west1-b'):
-    print(instance['name'])
 
-create_firewall_rule(service, project, fwPort, fwName)
+def create_instance(compute, project, zone, name, nodeType):
+    image_response = (
+        compute.images()
+        .getFromFamily(project="ubuntu-os-cloud", family="ubuntu-2204-lts")
+        .execute()
+    )
+    source_disk_image = image_response["selfLink"]
 
+    # Configure the machine
+    machine_type = f"zones/{zone}/machineTypes/{nodeType}"
+    startup_script = open(
+        os.path.join(os.path.dirname(__file__), "startup-script.sh")
+    ).read()
+
+    config = {
+        "name": name,
+        "machineType": machine_type,
+        # Specify the boot disk and the image to use as a source.
+        "disks": [
+            {
+                "boot": True,
+                "autoDelete": True,
+                "initializeParams": {
+                    "sourceImage": source_disk_image,
+                },
+            }
+        ],
+        # Specify a network interface with NAT to access the public
+        # internet.
+        "networkInterfaces": [
+            {
+                "network": "global/networks/default",
+                "accessConfigs": [{"type": "ONE_TO_ONE_NAT", "name": "External NAT"}],
+            }
+        ],
+        # Allow the instance to access cloud storage and logging.
+        "serviceAccounts": [
+            {
+                "email": "default",
+                "scopes": [
+                    "https://www.googleapis.com/auth/devstorage.read_write",
+                    "https://www.googleapis.com/auth/logging.write",
+                ],
+            }
+        ],
+    }
+
+    return compute.instances().insert(project=project, zone=zone, body=config).execute()
+
+
+def wait_for_operation(compute,project,zone,operation):
+    print("Waiting for operation to finish...")
+    while True:
+        result = (
+            compute.zoneOperations()
+            .get(project=project, zone=zone, operation=operation)
+            .execute()
+        )
+
+        if result["status"] == "DONE":
+            print("done.")
+            if "error" in result:
+                raise Exception(result["error"])
+            return result
+
+        time.sleep(1)
+
+
+credentials, project = google.auth.default()
+compute = googleapiclient.discovery.build('compute', 'v1', credentials=credentials)
+zone = "us-west1-b"
+fwPort = 5000
+fwName = 'allow-{}'.format(fwPort)
+instanceName = 'blog'
+nodeType = 'e2-micro'
+
+print("Creating instance.")
+
+# Create and wait for instance creation
+operation = create_instance(compute, project, zone, instanceName, nodeType)
+wait_for_operation(compute, project, zone, operation["name"])
+
+# Print the instance
 try:
-    operation = create_instance(service, project, zone, instanceName,
-        nodeType = nodeType,
-        scriptFile = 'startup-script.sh',
-        fwName = fwName)
-    wait_for_operation(service, project, zone, operation['name'])
-except googleapiclient.errors.HttpError as exp:
-    print("An instance may already be running")
-    print(exp)
-
-try:
-    instanceResponse = service.instances().get(project=project, zone=zone, instance=instanceName).execute()
+    instanceResponse = compute.instances().get(project=project, zone=zone, instance=instanceName).execute()
     if instanceResponse:
         instanceIP = instanceResponse['networkInterfaces'][0]['accessConfigs'][0]['natIP']
         print(f"The blog is runnin at http://{instanceIP}:{fwPort}")
 except googleapiclient.errors.HttpError as exp:
     print(f"Unable to locate instance {instanceName} to get the IP address")
     print(exp)
+
+#create_firewall_rule(service, project, fwPort, fwName)
 
